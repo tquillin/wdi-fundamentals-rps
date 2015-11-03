8ac44c5bbe7d49728cf180872eed75a4aea8d475
Terrell Quillin <QT@Terrells-MacBook-Pro.local>
2015-11-03 13:41:42 -0500

    Added expression to operate var move for player and computer

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -25,14 +25,17 @@ function getPlayerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
+    return var move || getInput; 
+    move = move || getInput(); 
+
 }
 
 function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return /* Your Expression */;
+    return return var move || randomPlay; 
+    move = move || randomPlay();
 }
 
 function getWinner(playerMove,computerMove) {
